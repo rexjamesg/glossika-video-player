35625006a4c4fdd2a14765127c91a3fd2b723e7a
林於豊 <rexjamesg@gmail.com>
2025-05-13 16:26:58 +0800

    init commit

---
+++ GlossikaVideoPlayer/GlossikaVideoPlayer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GlossikaVideoPlayer
+//
+//  Created by Yu Li Lin on 2025/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlossikaVideoPlayer/GlossikaVideoPlayer/GlossikaVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlossikaVideoPlayerApp.swift
+//  GlossikaVideoPlayer
+//
+//  Created by Yu Li Lin on 2025/5/13.
+//
+
+import SwiftUI
+
+@main
+struct GlossikaVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
